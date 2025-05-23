8401e10e66a0f26f00e1883c1ffd3d6cca55daf1
Dhanushka Nuwan Premasiri <97797596+DhanushkaNP@users.noreply.github.com>
2025-05-23 23:24:05 +0530

    fix

--- profile-view-script.js
+++ profile-view-script.js
@@ -400,7 +400,7 @@ document.addEventListener("DOMContentLoaded", async function () {
 
                 // Initialize Swiper after DOM is fully loaded
                 loadSwiperJS().then(() => {
-                  new Swiper(notablecasewinscontainer, {
+                  new Swiper(certicateContainer, {
                     slidesPerView: 1.2,
                     spaceBetween: 16,
                     slidesOffsetAfter: 30,
